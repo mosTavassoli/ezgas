0cb8304bac7cc3e38f3aeb9f90e1a7ce9ddd8509
ALESSANDRO RICCIUTO s270206 <s270206@studenti.polito.it>
2020-05-06 19:20:51 +0200

    feat: add service to getAllGasStations

--- src/main/java/it/polito/ezgas/service/impl/GasStationServiceimpl.java
+++ src/main/java/it/polito/ezgas/service/impl/GasStationServiceimpl.java
@@ -2,6 +2,7 @@ package it.polito.ezgas.service.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import exception.GPSDataException;
@@ -9,14 +10,24 @@ import exception.InvalidGasStationException;
 import exception.InvalidGasTypeException;
 import exception.InvalidUserException;
 import exception.PriceException;
+import it.polito.ezgas.converter.GasStationConverter;
 import it.polito.ezgas.dto.GasStationDto;
+import it.polito.ezgas.entity.GasStation;
+import it.polito.ezgas.repository.GasStationRepository;
 import it.polito.ezgas.service.GasStationService;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Created by softeng on 27/4/2020.
  */
 @Service
 public class GasStationServiceimpl implements GasStationService {
+	
+	@Autowired
+	GasStationRepository gasStationRepository;
+	Logger logger = Logger.getLogger(GasStationServiceimpl.class.getName());
 
 	@Override
 	public GasStationDto getGasStationById(Integer gasStationId) throws InvalidGasStationException {
@@ -26,14 +37,14 @@ public class GasStationServiceimpl implements GasStationService {
 
 	@Override
 	public GasStationDto saveGasStation(GasStationDto gasStationDto) throws PriceException, GPSDataException {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public List<GasStationDto> getAllGasStations() {
-		// TODO Auto-generated method stub
-		return null;
+		logger.log(Level.INFO, "GET - all gas stations");
+		List<GasStation> gasStations = gasStationRepository.findAll();
+		return GasStationConverter.toDto(gasStations);
 	}
 
 	@Override
