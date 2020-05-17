1865c22123f9f697d0cb799c05561950952caa15
Mostafa Tavassoli <m.Tavassoli@asus.local>
2020-05-17 14:16:27 +0200

    test: add unit tests for User methods setUserId() and getUserId()

--- src/test/java/it/polito/ezgas/UserTest.java
+++ src/test/java/it/polito/ezgas/UserTest.java
@@ -4,11 +4,77 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import it.polito.ezgas.entity.User;
+
 public class UserTest {
 
+User user;
+	
+	private Integer userId = 15;
+	private String userName = "Username";
+	private String password = "pass";
+	private String email = "test@email.com";
+	private Integer reputation = 5;
+	private Boolean admin = false;
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testUserIdMinInt() {
+		user = new User();
+		user.setUserId(Integer.MIN_VALUE);
+		assertTrue(user.getUserId() == Integer.MIN_VALUE);
+	}
+	
+	@Test
+	public void testUserIdMinIntPlusOne() {
+		user = new User();
+		user.setUserId(Integer.MIN_VALUE + 1);
+		assertTrue(user.getUserId() == Integer.MIN_VALUE + 1 );
+	}
+	
+	@Test
+	public void testUserIdMinusOne() {
+		user = new User();
+		user.setUserId(-1);
+		assertTrue(user.getUserId() == -1);
+	}
+	
+	@Test
+	public void testUserIdZero() {
+		user = new User();
+		user.setUserId(0);
+		assertTrue(user.getUserId() == 0);
+	}
+	
+	@Test
+	public void testUserIdMaxInt() {
+		user = new User();
+		user.setUserId(Integer.MAX_VALUE);
+		assertTrue(user.getUserId() == Integer.MAX_VALUE);
+	}
+	
+	@Test
+	public void testUserIdMaxIntMinusOne() {
+		user = new User();
+		user.setUserId(Integer.MAX_VALUE -1);
+		assertTrue(user.getUserId() == Integer.MAX_VALUE -1);
+	}
+	
+	
+	@Test
+	public void testUserEntityConstructor() {
+		user = new User(this.userName,this.password,this.email,this.reputation);
+		assertNull(user.getUserId());
+		assertTrue(user.getUserName() == this.userName);
+		assertTrue(user.getPassword() == this.password);
+		assertTrue(user.getEmail() == this.email);
+		assertTrue(user.getReputation() == this.reputation);
+	}
+	
+	
+	@Test
+	public void testUserName() {
+		user = new User(this.userName,this.password,this.email,this.reputation);
+		assertEquals(this.userName,this.user.getUserName());
 	}
 
 }
