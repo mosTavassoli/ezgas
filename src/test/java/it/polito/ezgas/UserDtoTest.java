416e398b8573a2e8d338268f352f5df695b62968
ALESSANDRO RICCIUTO s270206 <s270206@studenti.polito.it>
2020-05-17 13:21:00 +0200

    test: add test case testConstructor for UserDto constructor

--- src/test/java/it/polito/ezgas/UserDtoTest.java
+++ src/test/java/it/polito/ezgas/UserDtoTest.java
@@ -2,13 +2,32 @@ package it.polito.ezgas;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import it.polito.ezgas.dto.UserDto;
+
 public class UserDtoTest {
+	
+	private UserDto userDto;
+	
+	@Before
+	public void init() {
+		userDto = new UserDto();
+	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testConstructor() {
+		userDto = new UserDto(12, "username12", "pass12", "test12@example.com", 3, true);
+		
+		assertEquals((Integer)12, userDto.getUserId());
+		assertEquals("username12", userDto.getUserName());
+		assertEquals("pass12", userDto.getPassword());
+		assertEquals("test12@example.com", userDto.getEmail());
+		assertEquals((Integer)12, userDto.getReputation());
+		assertEquals(true, userDto.getAdmin());
 	}
+	
+
 
 }
