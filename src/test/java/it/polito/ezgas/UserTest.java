4864bfc93357db4bd23339068a24d3a9b900f8fc
Mostafa Tavassoli <m.Tavassoli@asus.local>
2020-05-18 00:45:37 +0200

    test: fix unit test for User constructor

--- src/test/java/it/polito/ezgas/UserTest.java
+++ src/test/java/it/polito/ezgas/UserTest.java
@@ -9,22 +9,32 @@ import it.polito.ezgas.entity.User;
 
 public class UserTest {
 	User user;
-	
+	 
 	@Before
 	public void init() {
 		this.user = new User();
 	}
 	
 	private Integer userId = 15;
-	private String userName = "Username";
-	private String password = "pass";
-	private String email = "test@email.com";
-	private Integer reputation = 5;
+	private String userName = "Username15";
+	private String password = "pass15";
+	private String email = "test15@email.com";
+	private Integer reputation = 3;
 	private Boolean admin = false;
 	
 	@Test
+	public void testUserEntityConstructor() {
+		user = new User(this.userName,this.password,this.email,this.reputation);
+		assertNull(user.getUserId());
+		assertTrue(user.getUserName() == this.userName);
+		assertTrue(user.getPassword() == this.password);
+		assertTrue(user.getEmail() == this.email);
+		assertTrue(user.getReputation() == this.reputation);
+	}
+	
+	
+	@Test
 	public void testUserIdMinInt() {
-		user = new User();
 		user.setUserId(Integer.MIN_VALUE);
 		assertTrue(user.getUserId() == Integer.MIN_VALUE);
 	}
@@ -59,22 +69,5 @@ public class UserTest {
 		assertTrue(user.getUserId() == Integer.MAX_VALUE -1);
 	}
 	
-	
-	@Test
-	public void testUserEntityConstructor() {
-		user = new User(this.userName,this.password,this.email,this.reputation);
-		assertNull(user.getUserId());
-		assertTrue(user.getUserName() == this.userName);
-		assertTrue(user.getPassword() == this.password);
-		assertTrue(user.getEmail() == this.email);
-		assertTrue(user.getReputation() == this.reputation);
-	}
-	
-	
-	@Test
-	public void testUserName() {
-		user = new User(this.userName,this.password,this.email,this.reputation);
-		assertEquals(this.userName,this.user.getUserName());
-	}
 
 }
