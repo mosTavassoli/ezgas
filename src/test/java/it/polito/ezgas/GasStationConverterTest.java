09114aab00ab36a6efc87f397abe6ebc0f3a218b
TONI SALIBA s275112 <s275112@studenti.polito.it>
2020-05-23 19:05:26 +0300

    test: add class GasStationConverterTest

---
+++ src/test/java/it/polito/ezgas/GasStationConverterTest.java
@@ -0,0 +1,16 @@
+package it.polito.ezgas;
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import it.polito.ezgas.converter.GasStationConverter;
+import it.polito.ezgas.dto.GasStationDto;
+import it.polito.ezgas.entity.GasStation;
+public class GasStationConverterTest {
+
+	
+	
+}
