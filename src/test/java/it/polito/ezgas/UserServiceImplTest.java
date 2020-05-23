d7496e04e55b906cf5d05680c7a1201b16c40625
Personal <m.tavassoli89@gmail.com>
2020-05-23 13:22:03 +0200

    test: add class UserServiceImplTest

---
+++ src/test/java/it/polito/ezgas/UserServiceImplTest.java
@@ -0,0 +1,21 @@
+package it.polito.ezgas;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import static org.mockito.Mockito.*;
+
+import java.util.ArrayList;
+import it.polito.ezgas.dto.UserDto;
+import it.polito.ezgas.entity.User;
+import it.polito.ezgas.repository.UserRepository;
+import it.polito.ezgas.service.impl.UserServiceimpl;
+
+public class UserServiceImplTest {
+	
+	
+
+}
