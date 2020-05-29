5a5c208232c920da07ea872e804faf0cc390ef50
MEHDI KHRICHFA s280064 <s280064@studenti.polito.it>
2020-05-29 12:22:07 +0200

    test: add TestController class

---
+++ src/test/java/it/polito/ezgas/controllertests/TestController.java
@@ -0,0 +1,14 @@
+package it.polito.ezgas.controllertests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestController {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
