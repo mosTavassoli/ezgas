c7027b56071913f8dc109c7fe7a511c923ada3f8
TONI SALIBA s275112 <s275112@studenti.polito.it>
2020-06-01 18:22:49 +0300

    test: add GUI test for scenario uc2

---
+++ GUITests/testScenarioUC2_tony.sikuli/testScenarioUC2.py
@@ -0,0 +1,12 @@
+click("1590925845726.png")
+click("1591022763316.png")
+doubleClick("1591022763316.png")
+write("tonySaliba")
+click("1590926034269.png")
+if exists("1590930016121.png"):
+    popup("Success!", "Test")
+else:
+    popup("Failure!", "Test")
+    
+
+
