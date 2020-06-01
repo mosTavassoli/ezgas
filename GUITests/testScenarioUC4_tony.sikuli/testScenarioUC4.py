4bd78ae4ea7732bf1921f4314b0a915589f7ab1b
TONI SALIBA s275112 <s275112@studenti.polito.it>
2020-06-01 18:21:20 +0300

    test: add GUI test for scenario uc4

---
+++ GUITests/testScenarioUC4_tony.sikuli/testScenarioUC4.py
@@ -0,0 +1,20 @@
+click("1590932960796.png")
+
+while(1):
+    type(Key.PAGE_DOWN) 
+    if exists("1590933395343.png"):
+        break;
+type("1590933100998.png", "total")
+type("1590933445697.png", "mrouj")
+click("1590933602059.png")
+click("1590933985236.png")
+click("1590934001733.png")
+while(1):
+    type(Key.PAGE_DOWN) 
+    if exists("1590934080164.png"):
+        popup("Success!", "Test")
+        break;
+    if exists("1590934140788.png"):
+        popup("Failure!", "Test")
+        break;
+    
