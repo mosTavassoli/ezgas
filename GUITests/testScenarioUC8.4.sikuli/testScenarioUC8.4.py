893cf7581e6a0084010e32392234ed221f9da3c9
TONI SALIBA s275112 <s275112@studenti.polito.it>
2020-06-02 12:06:36 +0300

    test: add changes to GUI tests UC8.4

--- GUITests/testScenarioUC8.4.sikuli/testScenarioUC8.4.py
+++ GUITests/testScenarioUC8.4.sikuli/testScenarioUC8.4.py
@@ -5,7 +5,7 @@ while(1):
         break;
 type("1590935479504.png", "mrouj")
 click("1590935585144.png")
-click("1590935636940.png")
+click("1591088709368.png")
 click("1590935651914.png")
 click("1590935754198.png")
 i = 0
